b96560254a2dcede10c82ac5819e59137b4c7e4e
Awe4k <ragnarok666@mail.ru>
2015-01-22 04:05:44 +0300

    Fin commit

--- src/main/java/com/javacat/MainClass.java
+++ src/main/java/com/javacat/MainClass.java
@@ -2,6 +2,7 @@ package com.javacat;
 
 public class MainClass {
     public static void main(String[] args) {
+        System.out.println("Enter correct expression. To stop work type \"quit\"");
         Calculator.startCalculations();
     }
 }
